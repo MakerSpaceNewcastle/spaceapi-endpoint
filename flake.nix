e26858651907a81459c6b97d89f5a0a9864854a7
Dan Nixon <dan@dan-nixon.com>
2026-01-02 10:26:51 +0000

    Update flake inputs

--- flake.nix
+++ flake.nix
@@ -1,35 +1,32 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
-    self,
     nixpkgs,
     flake-utils,
+    ...
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = (import nixpkgs) {
           inherit system;
         };
-
-        lintingRustFlags = "-D unused-crate-dependencies";
       in {
         devShell = pkgs.mkShell {
           packages = with pkgs; [
+            # Rust toolchain
             rustup
-            nodePackages.wrangler
-            worker-build
             wasm-pack
+            worker-build
+            wrangler
 
             # Code formatting tools
             alejandra
             treefmt
           ];
-
-          RUSTFLAGS = lintingRustFlags;
         };
       }
     );
